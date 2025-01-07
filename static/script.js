25d4ce1ebfe1d716f792d1c0401b19442fd80fa9
Nikita <nkolosov08@icloud.com>
2025-01-07 13:05:55 -0800

    code it so buttons on cards page are disabled after one click

--- static/script.js
+++ static/script.js
@@ -13,12 +13,15 @@ $(document).ready(function() {
 	
 	$("#buttonS").click(function() {
 		$("#randomS").text(suspects[Math.floor(Math.random() * suspects.length)]);
+		$("#buttonS").addClass("disabled");
 	});
 	$("#buttonW").click(function() {
 		$("#randomW").text(weapons[Math.floor(Math.random() * weapons.length)]);
+		$("#buttonW").addClass("disabled");
 	});
 	$("#buttonR").click(function() {
 		$("#randomR").text(rooms[Math.floor(Math.random() * rooms.length)]);
+		$("#buttonR").addClass("disabled");
 	});
 
 
